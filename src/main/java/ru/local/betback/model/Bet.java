2bef5ad8162fd2fa74197e5c120f261b64fd8e5b
NiceWone <gowa36484@gmail.com>
2018-07-01 16:10:52 +0300

    - test front with bets entity

--- src/main/java/ru/local/betback/model/Bet.java
+++ src/main/java/ru/local/betback/model/Bet.java
@@ -1,24 +1,24 @@
-//package ru.local.betback.model;
-//
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Data
-//@NoArgsConstructor
-//public class Bet {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @OneToOne
-//    private BetType betType;
-//    @ManyToOne
-//    private Match match;
-//    @OneToOne
-//    private User user;
-//    private Double bet;
-//}
+package ru.local.betback.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Bet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private BetType betType;
+    @ManyToOne
+    private Match match;
+    @OneToOne
+    private User user;
+    private Double bet;
+}
--- src/main/java/ru/local/betback/repository/BetRepository.java
+++ src/main/java/ru/local/betback/repository/BetRepository.java
@@ -1,11 +1,11 @@
-//package ru.local.betback.repository;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import ru.local.betback.model.Bet;
-//
-//import java.util.List;
-//
-//public interface BetRepository extends CrudRepository<Bet, Long> {
-//
-//    List<Bet> findAll();
-//}
+package ru.local.betback.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.local.betback.model.Bet;
+
+import java.util.List;
+
+public interface BetRepository extends CrudRepository<Bet, Long> {
+
+    List<Bet> findAll();
+}
